d6ddac279e8b39be7b3c83bebb116b2545effbc3
William Frank Monroy Mamani <william_monroy777@hotmail.com>
2023-07-20 11:32:44 -0600

    Initial Commit

---
+++ hack-ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  hack-ios
+//
+//  Created by William Frank Monroy Mamani on 20/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hack-ios/hack_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hack_iosApp.swift
+//  hack-ios
+//
+//  Created by William Frank Monroy Mamani on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct hack_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
