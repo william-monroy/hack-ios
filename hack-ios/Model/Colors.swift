1741b5f7460f2925d8692fd34475b876abba0833
Bren Saucedo <73083907+Bren12@users.noreply.github.com>
2023-07-20 21:56:29 -0500

    login-connected

---
+++ hack-ios/Model/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  hack-ios
+//
+//  Created by Brenda Saucedo on 20/07/23.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct Colors {
+    static let lightOrange = Color(red: 227/255, green: 190/255, blue: 156/255)
+}
