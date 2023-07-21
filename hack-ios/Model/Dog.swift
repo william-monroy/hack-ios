705c684d3382df6106c8316863c5a5fa8e1ffd52
Bren Saucedo <73083907+Bren12@users.noreply.github.com>
2023-07-20 22:43:42 -0500

    location-annotations

--- hack-ios/Model/Dog.swift
+++ hack-ios/Model/Dog.swift
@@ -21,8 +21,8 @@ struct Dog: Identifiable {
 
 extension Dog {
     static var dogs : [Dog] = [
-        Dog(image: "Dachshund", breed: "Dachshund", color: ["Brown"], size: "Small", location: "Near Oakwood Park", picked: false, date: Date(), description: "Has a distinctive red coat with black markings along her back and tail. Her endearing short legs and long body make her stand out from other dogs."),
-        Dog(image: "Shitzu", breed: "Shitzu", color: ["White", "Gray"], size: "Small", location: "Beach", picked: true, date: Date(), description: "Small, fluffy dog with a luxurious coat that's a mix of gray and white colors, beautifully complemented by their expressive dark eyes."),
-        Dog(image: "Pug", breed: "Pug", color: ["Black", "White", "Brown"], size: "Small", location: "City", picked: false, date: Date(), description: "This Pug is a small, sturdy dog with a distinct wrinkled face, short muzzle, and charming round eyes. Their coat is smooth and can vary in color, often seen in fawn, black, or silver."),
+        Dog(image: "Dachshund", breed: "Dachshund", color: ["Brown"], size: "Small", location: "Valparaíso", picked: false, date: Date(), description: "Has a distinctive red coat with black markings along her back and tail. Her endearing short legs and long body make her stand out from other dogs."),
+        Dog(image: "Shitzu", breed: "Shitzu", color: ["White", "Gray"], size: "Small", location: "Tec de Monterrey", picked: true, date: Date(), description: "Small, fluffy dog with a luxurious coat that's a mix of gray and white colors, beautifully complemented by their expressive dark eyes."),
+        Dog(image: "Pug", breed: "Pug", color: ["Black", "White"], size: "Small", location: "Av. Luis Elizondo", picked: false, date: Date(), description: "This Pug is a small, sturdy dog with a distinct wrinkled face, short muzzle, and charming round eyes. Their coat is smooth and can vary in color, often seen in fawn, black, or silver."),
     ]
 }
